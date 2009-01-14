41cb9c4d1dff0fc15d94a920dd59003f23c106a5
Maarten Billemont <lhunath@gmail.com>
2009-01-14 12:10:09 +0100

    Artwork update.
        - Add screenshots, H264 encoded movie.
        - Update icon & iTunesArtwork.
        - Update website:
            - Can now view movie through YouTube on iPhone.
            - Added screenshots.
            - Fixed preload link.
        - Add Google Sitemap & Robots file.

--- index.php
+++ index.php
@@ -3,20 +3,33 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
         <title>Gorillas - The iPhone Game</title>
+        <meta name="verify-v1" content="IluFmv0cMN6OlW/zGTTUB9T7yXMyqW2PiEA5scdp274=" />
+        <link rel="icon" href="favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="favicon.png" type="image/png" />
         <link rel="stylesheet" type="text/css" href="style.css" />
-        <script type="text/javascript" src=".jquery/jquery.js"></script>
+        <!--[if !IE]>-->
+            <link type="text/css" media="only screen and (max-device-width: 480px)" href="style-iphone.css" rel="stylesheet" />
+        <!--<![endif]-->
+        <!--[if lt IE 8]>
+            <script type="text/javascript">
+                IE7_PNG_SUFFIX = ".png";
+            </script>
+            <script type="text/javascript" src="http://ie7-js.googlecode.com/svn/version/2.0(beta3)/IE8.js"></script>
+        <![endif]-->
         <script type="text/javascript">
-            $(document).ready(function() {
- 
-                $("#preview").toggle(
-                    function () {
-                        $("object").css({"display":"block"});
-                    },
-                    function () {
-                        $("object").css({"display":"none"});
-                    }
-                );
-            });
+            function showMovie() {
+                var movieElement = document.getElementById('preview').getElementsByTagName('object')[0];
+
+                if(movieElement.style.display == 'block') {
+                    movieElement.style.display = 'none';
+                    pageTracker._trackEvent("Movies", "Start", "gorillas.flv", 1);
+                } else {
+                    movieElement.style.display = 'block';
+                    pageTracker._trackEvent("Movies", "Stop", "gorillas.flv", 1);
+                }
+
+                return true;
+            }
         </script>
     </head>
 
@@ -28,22 +41,22 @@
         </div> 
             
         <div id="preview">
-            <!--img src="images/gorillas_phone.png" alt="Screenshot of the Gorillas game on the iPhone." /-->
             <div class="tip">Click for a short gameplay video.</div>
             <object type="application/x-shockwave-flash" data=".jquery/shadowbox-2.0/flvplayer.swf">
                 <param name="movie" value=".jquery/shadowbox-2.0/flvplayer.swf" />
                 <param name="allowfullscreen" value="true" />
                 <param name="flashvars" value="file=http://gorillas.lyndir.com/gorillas.flv&amp;width=482&amp;height=322&amp;autostart=true&amp;displayheight=322&amp;showicons=false&amp;backcolor=0x000000&amp;frontcolor=0xCCCCCC&amp;lightcolor=0x557722" />
             </object>
-            <!--a rel="shadowbox;width=482;height=302" title="Gorillas" href="gorillas.flv">
-                <img src="images/gorillas_phone.png" />
-            </a-->
+            <a href="http://www.youtube.com/watch?v=Hu4Y8eJLqkI" onclick="return pageTracker._trackEvent('Movies', 'Start', 'gorillas.youtube', 1);"></a>
         </div>
 
         <script type="text/javascript">
             var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
             document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
         </script><script type="text/javascript">
+            if(navigator.appVersion.indexOf('iPhone') == -1)
+                document.getElementById('preview').onclick = showMovie;
+
             try {
                 var pageTracker = _gat._getTracker("UA-90535-5");
                 pageTracker._trackPageview();
@@ -52,34 +65,45 @@
 
         <div id="content">
             <!-- AddThis -->
-            <script type="text/javascript">
-                addthis_pub             = 'lhunath'; 
-                addthis_logo            = 'http://gorillas.lyndir.com/images/icon.png';
-                addthis_logo_background = 'EFEFFF';
-                addthis_logo_color      = '666699';
-                addthis_brand           = 'Gorillas';
-                addthis_options         = 'email, twitter, stumbleupon, digg, delicious, facebook, slashdot, more';
-            </script>
-            <a href="http://www.addthis.com/bookmark.php"
-                onmouseover="return addthis_open(this, '', '[URL]', '[TITLE]')"
-                onclick="return addthis_sendto()"
-                onmouseout="addthis_close()">
-                <img src="images/sm-share-en.gif" alt="Share" />
-            </a><br />
-            <script type="text/javascript" src="http://s7.addthis.com/js/152/addthis_widget.js"></script>
+            <p>
+                <script type="text/javascript">
+                    addthis_pub             = 'lhunath'; 
+                    addthis_logo            = 'http://gorillas.lyndir.com/images/icon.png';
+                    addthis_logo_background = 'EFEFFF';
+                    addthis_logo_color      = '666699';
+                    addthis_brand           = 'Gorillas';
+                    addthis_options         = 'email, twitter, stumbleupon, digg, delicious, facebook, slashdot, more';
+                </script>
+                <a href="http://www.addthis.com/bookmark.php"
+                    onmouseover="return addthis_open(this, '', '[URL]', '[TITLE]')"
+                    onclick="return addthis_sendto()"
+                    onmouseout="addthis_close()">
+                    <img src="images/sm-share-en.gif" alt="Share" />
+                </a>
+                <script type="text/javascript" src="http://s7.addthis.com/js/152/addthis_widget.js"></script>
+            </p>
+
+            <p>
+                <img class="screenshot" src="images/Screenshot 2009.01.13 21.43.25.png" />
+                <img class="screenshot" src="images/Screenshot 2009.01.13 21.25.33.png" />
+                <img class="screenshot" src="images/Screenshot 2009.01.13 21.10.41.png" />
+                <img class="screenshot" src="images/Screenshot 2009.01.13 21.12.30.png" />
+            </p>
 
             <!-- W3C, Valid XHTML 1.0 -->
-            <a href="http://validator.w3.org/check?uri=referer">
-                <img src="images/valid-xhtml10.png" alt="Valid XHTML 1.0 Strict" />
-            </a><br />
+            <p>
+                <a href="http://validator.w3.org/check?uri=referer">
+                    <img src="images/valid-xhtml10.png" alt="Valid XHTML 1.0 Strict" />
+                </a>
+            </p>
 
             <!-- Creative Commons License -->
             <a href="http://creativecommons.org/licenses/GPL/2.0/" class="float right">
                 <img alt="CC-GNU GPL" src="http://creativecommons.org/images/public/cc-GPL-a.png" />
-            </a><br />
+            </a>
 
             <!-- Image Preloading -->
-            <img src="images/back_hover trac_hover.png" class="preload" alt="Preload image of Trac logo." />
+            <img src="images/back_trac_hover.png" class="preload" alt="Preload image of Trac logo." />
         </div>
 
         <p id="footer">
