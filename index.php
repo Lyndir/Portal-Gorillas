6db1a60cf1578498d7af05b136de919d99ccbab9
Maarten Billemont <lhunath@gmail.com>
2009-02-25 08:27:07 +0100

    Skill & HUD revamp
        - Added feature: Skill
        - Skill is indicated on the HUD
        - HUD revamped a little using a new font
        - Split website up in different pages and added a menu

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+    $target = 'about';
+    if(isset($_GET['about']))
+        $target = 'about';
+    elseif(isset($_GET['original']))
+        $target = 'original';
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -44,111 +53,59 @@
     </head>
 
     <body>
+        <div>
+            <ul id="menu">
+                <li class="a<?=($target == 'about')? " active": ""?>">
+                    <a href="?about"></a></li>
+                <li class="b">
+                    <a href="/trac"></a></li>
+                <li class="c<?=($target == 'original')? " active": ""?>">
+                    <a href="?original"></a></li>
+                <li class="d">
+                    <a href="http://lhunath.lyndir.com"></a></li>
+            </ul>
+        </div>
+
         <div id="back">
             <a class="track" href="/trac"></a>
             <a class="title" href="/"></a>
             <div class="bottom"></div>
         </div> 
                 
-        <div id="preview">
-            <div class="tip">Click for a short gameplay video.<br />
-                <span class="small">(or <a href="http://www.youtube.com/watch?v=Hu4Y8eJLqkI">YouTube</a>
-                                      / <a href="gorillas.mp4">MPEG-4</a>)</span></div>
-            <object type="application/x-shockwave-flash" data=".jquery/shadowbox-2.0/flvplayer.swf">
-                <param name="movie" value=".jquery/shadowbox-2.0/flvplayer.swf" />
-                <param name="allowfullscreen" value="true" />
-                <param name="flashvars" value="file=http://gorillas.lyndir.com/gorillas.flv&amp;width=482&amp;height=322&amp;autostart=true&amp;displayheight=322&amp;showicons=false&amp;backcolor=0x000000&amp;frontcolor=0xCCCCCC&amp;lightcolor=0x557722" />
-            </object>
-            <a class="iphone" href="http://www.youtube.com/watch?v=Hu4Y8eJLqkI" onclick="return pageTracker._trackEvent('Movies', 'Start', 'gorillas.youtube', 1);"></a>
-        </div>
+        <? include("$target.php"); ?>
 
         <script type="text/javascript">
             var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
             document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
         </script><script type="text/javascript">
-            if(navigator.appVersion.indexOf('iPhone') == -1)
-                document.getElementById('preview').onclick = showMovie;
-
             try {
                 var pageTracker = _gat._getTracker("UA-90535-5");
                 pageTracker._trackPageview();
             } catch(err) {}
         </script>
 
-        <div id="content">
-            <p>
-                <!-- App Store -->
-                <a href="http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=302275459&mt=8">
-                    <img src="images/appstore.png" />
-                </a>
-                <br />
-
-                <!-- AddThis -->
-                <script type="text/javascript">
-                    addthis_pub             = 'lhunath'; 
-                    addthis_logo            = 'http://gorillas.lyndir.com/images/icon.png';
-                    addthis_logo_background = 'EFEFFF';
-                    addthis_logo_color      = '666699';
-                    addthis_brand           = 'Gorillas';
-                    addthis_options         = 'email, twitter, stumbleupon, digg, delicious, facebook, slashdot, more';
-                </script>
-                <a href="http://www.addthis.com/bookmark.php"
-                    onmouseover="return addthis_open(this, '', '[URL]', '[TITLE]')"
-                    onclick="return addthis_sendto()"
-                    onmouseout="addthis_close()">
-                    <img src="images/sm-share-en.gif" alt="Share" />
-                </a>
-                <script type="text/javascript" src="http://s7.addthis.com/js/152/addthis_widget.js"></script>
-            </p>
-
-            <!-- Description -->
-            <div class="text">
-                <p><b>Gorillas</b><br />
-                    by <a class="b" href="http://lhunath.lyndir.com">Lhunath</a></p>
-                <p><i>Gorillas</i> is a turn based single player and multiplayer game where the objective is to blast your opponent away using carefully aimed bananas.</p>
-
-                <p>With a single touch of the display you determine the direction and speed of your banana's throw.  Don't forget to take the wind into account!</p>
-
-
-                <p><b>FEATURES:</b>
-                <ul>
-                    <li>Classic retro look</li>
-                    <li>Eye candy worthy of your iPhone / iPod touch</li>
-                    <li> Wind and weather effects, such as rain and snow</li>
-                    <li> Challenge the AI or a friend</li>
-                    <li> Dynamically adjusted difficulty</li>
-                    <li> Several different cities / worlds</li>
-                    <li> Fully Open Source (GPLv2)</li>
-                </ul></p>
-            </div>
-
-            <!-- Screenshots -->
-            <p id="screenshots">
-                <img src="images/2.png" />
-                <img src="images/3.png" />
-                <img src="images/4.png" />
-                <img src="images/5.png" />
-            </p>
-
+        <p>
             <!-- W3C, Valid XHTML 1.0 -->
-            <p>
-                <a href="http://validator.w3.org/check?uri=referer">
-                    <img src="images/valid-xhtml10.png" alt="Valid XHTML 1.0 Strict" />
-                </a>
-            </p>
-
-            <!-- Creative Commons License -->
-            <a href="http://creativecommons.org/licenses/GPL/2.0/" class="float right">
-                <img alt="CC-GNU GPL" src="http://creativecommons.org/images/public/cc-GPL-a.png" />
+            <a href="http://validator.w3.org/check?uri=referer">
+                <img src="images/valid-xhtml10.png" alt="Valid XHTML 1.0 Strict" />
             </a>
 
             <!-- Image Preloading -->
             <img src="images/back_trac_hover.png" class="preload" alt="Preload image of Trac logo." />
-        </div>
+        </p>
 
-        <!-- Credits -->
         <p id="footer">
-            <b xmlns:dc="http://purl.org/dc/elements/1.1/" href="http://purl.org/dc/dcmitype/InteractiveResource" property="dc:title" rel="dc:type">Gorillas</b> by <a class="b" xmlns:cc="http://creativecommons.org/ns#" href="http://gorillas.lyndir.com" property="cc:attributionName" rel="cc:attributionURL">Maarten Billemont</a> is licensed under the <a class="b" href="http://creativecommons.org/licenses/GPL/2.0/">CC-GNU GPL</a> version 2.0.
+            <!-- Credits -->
+            <b  xmlns:dc="http://purl.org/dc/elements/1.1/">Gorillas</b>
+            by
+            <a  class="b"
+                href="http://gorillas.lyndir.com"
+                rel="cc:attributionURL"
+                xmlns:cc="http://creativecommons.org/ns#">Maarten Billemont</a>
+            is licensed under the
+            <a  class="b"
+                href="http://creativecommons.org/licenses/GPL/2.0/">CC-GNU GPL</a>
+            version 2.0.
         </p>
     </body>
 </html>
---
+++ original.php
@@ -0,0 +1,27 @@
+        <div id="content">
+            <p>
+                <!-- Flash Game -->
+                <object     id="game"
+                            classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
+                            codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0"
+                            height="350"
+                            width="640">
+                    <param  name="allowScriptAccess"    value="sameDomain" />
+                    <param  name="movie"                value="http://rickyroad.com/bf-analyze/miscmedia/Gorilla.swf" />
+                    <param  name="menu"                 value="false" />
+                    <param  name="quality"              value="low" />
+                    <param  name="bgcolor"              value="#000000" />
+                    <param  name="wmode"                value="transparent" />
+                </object>
+            </p>
+
+            <!-- Referer -->
+            <div class="text">
+                <p><b>Gorilla.Bas (Flash remake)</b><br />
+                    by <a class="b" href="http://rickyroad.com/games/gorillas">Ricky Road</a></p>
+                <p><i>Your mission</i> is to hit your opponent with the exploding banana by varying the angle and power of your throw,
+                taking into account wind speed, gravity, and the city skyline.</p>
+
+                <p>The wind speed is shown by a directional arrow at the bottom of the playing field, its length relative to its strength.</p>
+            </div>
+        </div>
