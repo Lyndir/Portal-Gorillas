3072878a0256b1ec7031c688d2c908b14e3c052f
Maarten Billemont <lhunath@gmail.com>
2009-04-05 22:54:48 +0200

    Web update for 1.1
        - Web structure refactoring for easier addition of versions.
        - Web information on the 1.1 release.
        - Fixed click-to-start of flash movies on Safari.

---
+++ 110/about.php
@@ -0,0 +1,115 @@
+        <h2><a href="http://en.wikipedia.org/wiki/Easter">Easter</a> is coming to Gorillas..</h2>
+
+        <div id="preview" class="v110">
+            <div class="tip">Click for a short gameplay video.<br />
+                <span class="small">(or <a href="http://www.youtube.com/watch?v=kOd6fI2Cm7c"
+                                            onclick="pageTracker._trackPageview('/movie/youtube-1.1');">YouTube</a>
+                                      / <a href="1.1/movies/gorillas.mp4"
+                                            onclick="pageTracker._trackPageview('/movie/gorillas-1.1.mp4');">MPEG-4</a>)</span></div>
+            <a class="iphone" href="http://www.youtube.com/watch?v=kOd6fI2Cm7c"
+                onclick="pageTracker._trackPageview('/movie/youtube-1.1');"></a>
+        </div>
+
+        <script type="text/javascript">
+            if(navigator.appVersion.indexOf('iPhone') == -1) {
+                var element = document.getElementById('preview');
+
+                element.onclick = function() { return showMovie('1.1'); };
+                if (element.captureEvents)
+                    element.captureEvents(Event.CLICK);
+            }
+        </script>
+
+        <div id="content">
+            <p>
+                <!-- App Store -->
+                <a href="http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=302275459&amp;mt=8"
+                    onclick="pageTracker._trackPageview(this.href.replace(/^[a-z]+:\/\//i, '/external/'));">
+                    <img src="images/appstore.png" alt="The Apple App Store" />
+                </a>
+                <br />
+
+                <!-- AddThis -->
+                <script type="text/javascript">
+                    addthis_pub             = 'lhunath'; 
+                    addthis_logo            = 'http://gorillas.lyndir.com/images/icon.png';
+                    addthis_logo_background = 'EFEFFF';
+                    addthis_logo_color      = '666699';
+                    addthis_brand           = 'Gorillas';
+                    addthis_options         = 'email, twitter, stumbleupon, digg, delicious, facebook, slashdot, more';
+                </script>
+                <a href="http://www.addthis.com/bookmark.php"
+                    onmouseover="return addthis_open(this, '', '[URL]', '[TITLE]')"
+                    onclick="return addthis_sendto()"
+                    onmouseout="addthis_close()">
+                    <img src="images/sm-share-en.gif" alt="Share" />
+                </a>
+                <script type="text/javascript" src="http://s7.addthis.com/js/152/addthis_widget.js"></script>
+            </p>
+
+            <!-- Description -->
+            <div class="text">
+                <p><b>Gorillas</b> - 1.1<br />
+                    by <a class="border" href="http://lhunath.lyndir.com"
+                    onclick="pageTracker._trackPageview(this.href.replace(/^[a-z]+:\/\//i, '/external/'));">Lhunath</a></p>
+                <p>Gorillas is a port of the <a class="border" href="?original">original GORILLA.BAS</a> to the iPhone platform.</p>
+
+                <p>It's a turn based single player and multi player game where the objective is to blast your opponent away using carefully aimed bananas.</p>
+
+                <p>With a single touch of the display you determine the direction and speed of your banana's throw.  Don't forget to take the wind into account!</p>
+
+                <p><b>FEATURES:</b></p>
+                <ul>
+                   <li>Eye candy worthy of your iPhone / iPod touch</li>
+                   <li>Wind and weather effects, such as rain and snow</li>
+                   <li>A range of excellent retro remixes</li>
+                   <li>Challenge the AI or a friend</li>
+                   <li>Dynamically adjusted or configurable difficulty</li>
+                   <li>Pick from several different cities / worlds</li>
+                   <li>Fully <a class="border" href="/trac">Open Source (GPLv2)</a></li>
+                </ul>
+
+                <p><b>NEW IN 1.1:</b><br />
+                <ul>
+                   <li>Sound effects, vibration and shaking</li>
+                   <li>Game modes: Dynamic, Team Game &amp; Last Man Standing</li>
+                   <li>Camera action and game field zooming</li>
+                   <li>Many gorillas together in the game</li>
+                   <li>Throwing, cheering and dancing animations</li>
+                   <li>Kill shot replays</li>
+                </ul>
+            </div>
+
+            <!-- Screenshots -->
+            <p id="screenshots">
+                <a href="1.1/images/2o.png" rel="shadowbox[1.1]" title="Throwing bananas is an easy, one-touch motion.">
+                    <img src="1.1/images/2r.png" alt="Throwing bananas is an easy, one-touch motion." /></a>
+                <a href="1.1/images/3o.png" rel="shadowbox[1.1]" title="Gorillas cheer and dance as they take out their enemies.">
+                    <img src="1.1/images/3r.png" alt="Gorillas cheer and dance as they take out their enemies." /></a>
+                <a href="1.1/images/4o.png" rel="shadowbox[1.1]" title="Buildings can't protect you forever!">
+                    <img src="1.1/images/4r.png" alt="Buildings can't protect you forever!" /></a>
+                <a href="1.1/images/5o.png" rel="shadowbox[1.1]" title="Messages indicate who did what to whom.">
+                    <img src="1.1/images/5r.png" alt="Messages indicate who did what to whom." /></a>
+                <a href="1.1/images/6o.png" rel="shadowbox[1.1]" title="Team Game: Face the silverbacks with your friends.">
+                    <img src="1.1/images/6r.png" alt="Team Game: Face the silverbacks with your friends." /></a>
+                <a href="1.1/images/7o.png" rel="shadowbox[1.1]" title="Last Man Standing: A deadly free-for-all.">
+                    <img src="1.1/images/7r.png" alt="Last Man Standing: A deadly free-for-all." /></a>
+                <a href="1.1/images/8o.png" rel="shadowbox[1.1]" title="The kill that ends the game is replayed in slow motion.">
+                    <img src="1.1/images/8r.png" alt="The kill that ends the game is replayed in slow motion." /></a>
+                <a href="1.1/images/9o.png" rel="shadowbox[1.1]" title="Team kills deduct from your score.">
+                    <img src="1.1/images/9r.png" alt="Team kills deduct from your score." /></a>
+                <a href="1.1/images/10o.png" rel="shadowbox[1.1]" title="Summer Theme">
+                    <img src="1.1/images/10r.png" alt="Summer Theme" /></a>
+                <a href="1.1/images/11o.png" rel="shadowbox[1.1]" title="Alien Retro Theme">
+                    <img src="1.1/images/11r.png" alt="Alien Retro Theme" /></a>
+                <a href="1.1/images/12o.png" rel="shadowbox[1.1]" title="Alien Skies Theme">
+                    <img src="1.1/images/12r.png" alt="Alien Skies Theme" /></a>
+                <a href="1.1/images/13o.png" rel="shadowbox[1.1]" title="Good Luck!">
+                    <img src="1.1/images/13r.png" alt="Good Luck!" /></a>
+            </p>
+
+            <!-- Creative Commons License -->
+            <a href="http://creativecommons.org/licenses/GPL/2.0/" class="float right">
+                <img alt="CC-GNU GPL" src="http://creativecommons.org/images/public/cc-GPL-a.png" />
+            </a>
+        </div>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
 
     $target = 'about';
     if(isset($_GET['about']))
@@ -7,14 +8,42 @@
         $target = 'original';
     elseif(isset($_GET['next']))
         $target = 'next';
+    elseif(isset($_GET['test']))
+        $target = 'test';
 
+    $version = '110';
+    if(isset($_GET['v']))
+        if($_GET['v'] == '1.0' || $_GET['v'] == '100')
+            $version = '100';
+        elseif($_GET['v'] == '1.1' || $_GET['v'] == '110')
+            $version = '110';
+
+    if($_POST['message']) {
+        $name = $_POST['name'];
+        $message = $_POST['message'];
+        if(!$name)
+            $name = "Anonymous";
+
+        $_SESSION['comments'][$target] = array( $name, $message );
+
+        $append = sprintf("%s: %s\n", $name, $_POST['message']);
+        $current = file_get_contents("$target.moderate.txt");
+        if(!strpos($current, $append)) {
+          $handle = fopen("$target.moderate.txt", "a");
+          fwrite($handle, $append);
+          fclose($handle);
+        }
+    } elseif ($_SESSION['comments'][$target])
+        list($name, $message) = $_SESSION['comments'][$target];
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
         <title>Gorillas - The iPhone Game</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
         <meta name="verify-v1" content="IluFmv0cMN6OlW/zGTTUB9T7yXMyqW2PiEA5scdp274=" />
+  
         <link rel="icon" href="favicon.png" type="image/x-png" />
         <link rel="shortcut icon" href="favicon.png" type="image/x-png" />
         <link rel="stylesheet" type="text/css" href="style.css" />
@@ -37,33 +66,79 @@
             </script>
             <script type="text/javascript" src="http://ie7-js.googlecode.com/svn/version/2.0(beta3)/IE8.js"></script>
         <![endif]-->
+        <script type="text/javascript" src="swfobject/swfobject.js"></script>
+        <script type="text/javascript" src=".jquery/jquery.js"></script>
+        <script type="text/javascript" src=".jquery/shadowbox-2.0/build/adapter/shadowbox-jquery.js"></script>
+        <script type="text/javascript" src=".jquery/shadowbox-2.0/build/shadowbox.js"></script>
         <script type="text/javascript">
+            Shadowbox.loadSkin('classic', '.jquery/shadowbox-2.0/src/skin');
+            Shadowbox.loadLanguage('en', '.jquery/shadowbox-2.0/build/lang');
+            Shadowbox.loadPlayer(['img', 'flv', 'qt', 'wmp', 'iframe'], '.jquery/shadowbox-2.0/build/player');
+
+            $(document).ready(function() {
+
+                var options = {
+                    handleOversize:     'drag',
+                    flvPlayer:          '.jquery/shadowbox-2.0/build/flvplayer.swf',
+                    ext:                { img: ['png', 'jpg', 'jpeg', 'gif', 'bmp', 'php'] }
+                };
+
+                Shadowbox.init(options);
+            });
 
             // Flash demonstrational movie.
-            function showMovie() {
-                var movieElement = document.getElementById('preview').getElementsByTagName('object')[0];
-
-                if(movieElement.style.display == 'block') {
-                    movieElement.style.display = 'none';
-                } else {
-                    movieElement.style.display = 'block';
-                    pageTracker._trackPageview('/movie/gorillas.flv');
+            function showMovie(version) {
+                var flashvars = {
+                    file:           "http://gorillas.lyndir.com/" + version + "/movies/gorillas.flv",
+                    width:          "482",
+                    height:         "322",
+                    autostart:      "true",
+                    displayheight:  "322",
+                    showicons:      "false",
+                    backcolor:      "0x000000",
+                    frontcolor:     "0xCCCCCC",
+                    lightcolor:     "0x557722"
+                };
+                var params = {
+                    menu:           "false"
                 }
 
-                return true;
+                var swfElement = document.getElementById("swf");
+                if(swfElement) {
+                    swfobject.removeSWF("swf");
+
+                } else {
+                    swfElement = document.createElement("div");
+                    swfElement.setAttribute("id", "swf");
+                    document.getElementById("preview").appendChild(swfElement);
+
+                    swfobject.embedSWF(".jquery/shadowbox-2.0/flvplayer.swf", "swf", "482", "322", "9.0.0", "swfobject/expressInstall.swf", flashvars, params, {id: "swf"});
+
+                    pageTracker._trackPageview("/movie/gorillas-" + version + ".flv");
+                }
+
+                return false;
             }
 
             // Flash game: Original Gorilla.Bas
             function showGame() {
-                var gameElement = document.getElementById('game').getElementsByTagName('object')[0];
+                var flashvars = false;
+                var params = {
+                    menu:           "false"
+                }
 
-                if(!(gameElement.style.display == 'block')) {
-                    gameElement.style.display = 'block';
+                var swfElement = document.getElementById("swf");
+                if(!swfElement) {
+                    swfElement = document.createElement("div");
+                    swfElement.setAttribute("id", "swf");
+                    document.getElementById("game").appendChild(swfElement);
+
+                    swfobject.embedSWF("http://rickyroad.com/miscmedia/Gorilla.swf", "swf", "640", "350", "9.0.0", "swfobject/expressInstall.swf", flashvars, params, {id: "swf"});
+
                     pageTracker._trackPageview('/game/original');
-                } else
-                    return false;
+                }
 
-                return true;
+                return false;
             }
 
             // Google Analytics
@@ -72,7 +147,7 @@
         </script>
     </head>
 
-    <body>
+    <body class="<?="t_$target"?>">
         <div>
             <ul id="menu">
                 <li class="a<?=($target == 'about')? " active": ""?>">
@@ -85,6 +160,12 @@
                     <a href="http://lhunath.lyndir.com"
                         onclick="pageTracker._trackPageview(this.href.replace(/^[a-z]+:\/\//i, '/external/'));"></a></li>
             </ul>
+            <ul id="submenu">
+                <li class="a<?=($target == 'about' && $version == '100')? " active": ""?>">
+                    <a href="/?about&amp;v=1.0"></a></li>
+                <li class="b<?=($target == 'about' && $version == '110')? " active": ""?>">
+                    <a href="/?about&amp;v=1.1"></a></li>
+            </ul>
         </div>
 
         <div id="back">
@@ -93,6 +174,44 @@
             <div class="bottom"></div>
         </div> 
                 
+        <div id="comments">
+            <?php
+                $lines = explode("\n", file_get_contents("$target.txt"));
+                foreach($lines as $line) {
+                    list($_name, $_message) = explode(':', $line, 2);
+                    $_message = trim($_message);
+                    $_name = trim($_name);
+
+                    if($_name == $name || !$_message)
+                        continue; ?>
+
+                    <br /><?=sprintf('"%s"', htmlspecialchars(ucfirst($_message)))?>
+                    <?php if($_name) { ?>
+                        <?=sprintf('~%s', htmlspecialchars(preg_replace('/(@|\Wat\W).*/', '', $_name)))?>
+                    <? }
+                }
+            ?>
+            <?php if($message) { ?>
+                <br /><?=sprintf('"%s"', htmlspecialchars(ucfirst($message)))?>
+                <?php if($name) { ?>
+                    <?=sprintf('~%s', htmlspecialchars(preg_replace('/(@|\Wat\W).*/', '', $name)))?>
+                <? } ?>
+            <? } ?>
+            
+            <form action="<?="?$target&amp;v=$version"?>" method="POST">
+                <p>Leave <b>your</b> impression!</p>
+                <p>Try to keep it short and sweet.</p>
+
+                <label for="user">Name or email:</label>
+                <input type="text" name="name" id="name" value="<?=htmlspecialchars($name)?>" />
+
+                <label for="message">Your impression:</label>
+                <input type="text" name="message" id="message" value="<?=htmlspecialchars($message)?>" />
+
+                <label for="post"></label>
+                <input type="submit" name="post" id="post" value="Post" />
+            </form>
+        </div>
         <? include("$target.php"); ?>
 
         <p>
@@ -117,12 +236,12 @@
             <!-- Credits -->
             <b  xmlns:dc="http://purl.org/dc/elements/1.1/">Gorillas</b>
             by
-            <a  class="b"
+            <a  class="border"
                 href="http://gorillas.lyndir.com"
                 rel="cc:attributionURL"
                 xmlns:cc="http://creativecommons.org/ns#">Maarten Billemont</a>
             is licensed under the
-            <a  class="b"
+            <a  class="border"
                 href="http://creativecommons.org/licenses/GPL/2.0/">CC-GNU GPL</a>
             version 2.0.
         </p>
