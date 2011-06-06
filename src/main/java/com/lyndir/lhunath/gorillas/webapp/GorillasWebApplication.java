9b0aca114fa82f2ecdf0d2747df3ecde8bb42ee7
Maarten Billemont <lhunath@gmail.com>
2011-06-06 20:35:10 +0200

    Strip fixes.

--- src/main/java/com/lyndir/lhunath/gorillas/webapp/GorillasWebApplication.java
+++ src/main/java/com/lyndir/lhunath/gorillas/webapp/GorillasWebApplication.java
@@ -16,8 +16,11 @@ package com.lyndir.lhunath.gorillas.webapp;
  *   limitations under the License.
  */
 
+import com.lyndir.lhunath.opal.system.logging.exception.InternalInconsistencyException;
+import com.lyndir.lhunath.opal.system.util.ObjectUtils;
 import com.lyndir.lhunath.portal.apps.webapp.AppsSession;
 import com.lyndir.lhunath.portal.apps.webapp.AppsWebApplication;
+import com.lyndir.lhunath.portal.webapp.model.StripItem;
 import org.apache.wicket.*;
 import org.apache.wicket.protocol.http.WebApplication;
 
@@ -30,6 +33,16 @@ import org.apache.wicket.protocol.http.WebApplication;
  * @author lhunath
  */
 public class GorillasWebApplication extends AppsWebApplication {
+
+    @Override
+    public StripItem getActiveItem() {
+
+        for (final StripItem stripItem : getStripItems().getObject())
+            if (ObjectUtils.isEqual( stripItem.getLink(), "http://gorillas.lyndir.com" ))
+                return stripItem;
+
+        throw new InternalInconsistencyException( "Active strip not found." );
+    }
 
     @Override
     public Session newSession(final Request request, final Response response) {
